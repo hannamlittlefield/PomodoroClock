243c67cee68738a1dbb0fbc223035e6a7e3767e5
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-02-22 11:19:32 -0500

    adding reset and startstop button

--- src/SessionTimer.js
+++ src/SessionTimer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default function SessionTimer(){
+    let timer = "25:00"
     return(
         <div id="timer-label">
             <p>Session</p>
-            <div id="time-left">mm:ss</div>
+            <div id="time-left">{timer}</div>
+            <button id="start_stop">&#10074;&#10074;</button>
+            <button id="reset">&#8634;</button>
+
         </div>
     )
 }
