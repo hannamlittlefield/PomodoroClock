40eba5945f448e5d8c6ef09728d10aa16c2f5cf9
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-02-23 09:36:51 -0500

    adding in reset function - will work when state is lifted

--- src/SessionTimer.js
+++ src/SessionTimer.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function SessionTimer(){
-    let timer = "25:00"
+    const [lengthDisplay, setLengthDisplay] = useState(25)
+    /* when app handles state, will be able to call setbreak setlength*/
+    const [breakDisplay, setBreakDisplay] = useState(5)
+
+    let timer = new Date(lengthDisplay).getTime();
+    function handleOnReset(){
+        setBreakDisplay(5)
+        setLengthDisplay(25)
+    }
     return(
         <div id="timer-label">
             <p>Session</p>
             <div id="time-left">{timer}</div>
             <button id="start_stop">&#10074;&#10074;</button>
-            <button id="reset">&#8634;</button>
+            <button 
+                id="reset"
+                onClick={handleOnReset}>&#8634;</button>
 
         </div>
     )
