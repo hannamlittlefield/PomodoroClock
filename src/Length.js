1dd140e56f14c1ada74930c2ffcfe0ebfd37b7d9
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-02-24 10:11:31 -0500

    lifting state to App

--- src/Length.js
+++ src/Length.js
@@ -1,36 +1,34 @@
 import React, { useState } from 'react';
 
-export default function Length(){
- const [lengthDisplay, setLengthDisplay] = useState(25)
-
- function handleOnDec(){
-     if(lengthDisplay >=2){
-        setLengthDisplay(lengthDisplay - 1)
-    }
-    else{
-    }
-}
-
- function handleOnInc(){
-     if(lengthDisplay <= 59){
-    setLengthDisplay(lengthDisplay + 1)
-     }
-     else{
-         
-     }
-}
-
+export default function Length() {
+    const [lengthDisplay, setLengthDisplay] = useState(25)
+    function handleLengthOnDec(){
+        if(lengthDisplay >=2){
+           setLengthDisplay(lengthDisplay - 1)
+       }
+       else{
+       }
+   }
+   
+    function handleLengthOnInc(){
+        if(lengthDisplay <= 59){
+       setLengthDisplay(lengthDisplay + 1)
+        }
+        else{
+   
+        }
+   }
     return(
         <div id="break-label">
             <p>Session Length</p>
             <button 
                 id="session-decrement"
-                onClick={handleOnDec}
+                onClick={handleLengthOnDec}
                 >&#xab;</button>
                 {lengthDisplay}
             <button 
                 id="session-increment"
-                onClick={handleOnInc}
+                onClick={handleLengthOnInc}
                 >&#xbb;</button>
         </div>
     )
