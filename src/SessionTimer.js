eddfbcc19bb11ba89d5a712f251c5a6d4d59ce4c
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-02-24 10:29:53 -0500

    lifting state to App

--- src/SessionTimer.js
+++ src/SessionTimer.js
@@ -1,19 +1,11 @@
 import React, {useState} from 'react'
 
-export default function SessionTimer(){
-    const [lengthDisplay, setLengthDisplay] = useState(25)
-    /* when app handles state, will be able to call setbreak setlength*/
-    const [breakDisplay, setBreakDisplay] = useState(5)
+export default function SessionTimer({handleOnReset}, {lengthDisplay}){
 
-    let timer = new Date(lengthDisplay).getTime();
-    function handleOnReset(){
-        setBreakDisplay(5)
-        setLengthDisplay(25)
-    }
     return(
         <div id="timer-label">
             <p>Session</p>
-            <div id="time-left">{timer}</div>
+            <div id="time-left">{lengthDisplay}:00</div>
             <button id="start_stop">&#10074;&#10074;</button>
             <button 
                 id="reset"
