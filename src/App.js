cc6cd27b2c8ca319c4cb21a43e63ec18127311df
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-03-23 18:09:28 -0400

    scrapping code and starting new

--- src/App.js
+++ src/App.js
@@ -6,84 +6,156 @@ import SessionTimer from './SessionTimer.js'
 import React, { useState } from 'react';
 
 export default function App() {
+  let countdown = 0; // variable to set/clear intervals
+  const [workTime, setWorkTime] = useState(25);
+  const [seconds, setSeconds] = useState(workTime*60)
+  const [breakTime, setBreakTime] = useState(5);
+  let isBreak = true;
+  let isPaused = true;
+  let increment = 1;
 
-  /* function(){
-    if({sessionDisplay} === 00:00){
-      setSessionDisplay(breakDisplay)
-      then add function for countdown here
+  
+  const status = document.getElementById("status");
+  const timerDisplay = document.getElementById("timerDisplay");
+  const startBtn = document.getElementById("start-btn");
+  const resetBtn = document.getElementById("reset");
+  const workMin = document.getElementById("work-min");
+  const breakMin = document.getElementById("break-min");
+  
+  const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
+  alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
+  
+  
+
+  const handleStart = () => {
+    clearInterval(countdown);
+    isPaused = !isPaused;
+    if (!isPaused) {
+      countdown = setInterval(timer, 1000);
     }
-    
-
-  }
-
-
-
-  */
-  const [breakDisplay, setBreakDisplay] = useState(5)
-  const [lengthDisplay, setLengthDisplay] = useState(25)
-
-  function handleBreakOnDec(){
-      if(breakDisplay >= 2){
-          setBreakDisplay(breakDisplay - 1)}
-      else{
-      }
-  }
-
-  function handleBreakOnInc(){
-      if(breakDisplay <=59){
-          setBreakDisplay(breakDisplay + 1)
-      }   
-      else{
-      }
-    }
-
-    function handleLengthOnDec(){
-        if(lengthDisplay >=2){
-           setLengthDisplay(lengthDisplay - 1)
-       }
-       else{
-       }
-   }
-   
-    function handleLengthOnInc(){
-        if(lengthDisplay <= 59){
-       setLengthDisplay(lengthDisplay + 1)
-        }
-        else{
-   
-        }
-   }
-
-  function handleOnReset(){
-    setBreakDisplay(5)
-    setLengthDisplay(25)
 }
 
+  /* EVENT LISTENERS FOR START AND RESET BUTTONS */
+  /*
+  resetBtn.addEventListener('click', () => {
+    clearInterval(countdown);
+    seconds = workTime * 60;
+    countdown = 0;
+    isPaused = true;
+    isBreak = true;
+  })
+  */
+  /* TIMER - HANDLES COUNTDOWN */
+  function timer() {
+    setSeconds(seconds - 1);
+
+      if (seconds < 0) {
+        clearInterval(countdown);
+        alarm.currentTime = 0;
+        alarm.play();
+        seconds = (isBreak ? breakTime : workTime) * 60;
+        isBreak = !isBreak;
+        countdown = setInterval(timer, 1000);
+    }
+    countdownDisplay();
+    console.log(seconds)
+  }
+  
+  function incrementBreak(){
+    setBreakTime(breakTime + 1)
+    // TODO userstories to limit breaktime >0
+  }
+
+  function decrementBreak(){
+    setBreakTime(breakTime - 1)
+    // TODO userstories to limit breaktime >0
+  }
+
+  function incrementWork(){
+    setWorkTime(workTime + 1)
+    setSeconds(workTime*60)
+    // TODO userstories to limit breaktime <60
+  }
+
+  function decrementWork(){
+    setWorkTime(workTime - 1)
+    setSeconds(workTime*60)
+    // TODO userstories to limit breaktime <60
+  }
+
+  /* UPDATE HTML CONTENT */
+  function countdownDisplay() {
+    console.log(document.getElementById("timerDisplay").innerText)
+    let minutes = Math.floor(seconds / 60);
+    let remainderSeconds = seconds % 60;
+    if (timerDisplay != null) timerDisplay.innerText = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  }
+  
+  function buttonDisplay() {
+    if (isPaused && countdown === 0) {
+      startBtn.innerText = "START";
+    } else if (isPaused && countdown !== 0) {
+      startBtn.innerText = "Continue"; 
+    } else {
+      startBtn.innerText = "Pause";
+    }
+  }
+  
+  function updateHTML() {
+    countdownDisplay();
+    /*buttonDisplay();
+    isBreak ? status.innerText = "Keep Working" : status.innerText = "Take a Break!";
+    workMin.innerText = workTime;
+    breakMin.innerText = breakTime;  */
+  }
+  
+  window.setInterval(updateHTML, 100);
+  
+  document.onclick = updateHTML;
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="title">Productive Pomodoro</h1>
-      <SessionTimer
-        handleOnReset={handleOnReset}
-        lengthDisplay={lengthDisplay}
-        />
-      <Grid
-        container
-        direction= "row"
-        justify="space-evenly"
-        alignItems="center">
-          <Break
-            breakDisplay={breakDisplay}
-            handleBreakOnDec={handleBreakOnDec}
-            handleBreakOnInc={handleBreakOnInc}
-          />
-          <Length
-            lengthDisplay={lengthDisplay}
-            handleLengthOnDec={handleLengthOnDec}
-            handleLengthOnInc={handleLengthOnInc}
-          />
-      </Grid>
-      </header>
+    <div className="container">
+      <h1>Pomodoro Timer</h1>
+      
+      <div id="pomodoro">  
+        <div id="status"></div>
+        <div id="timerDisplay">{workTime}:00</div>
+        <button 
+          id="start-btn" 
+          className="btn"
+          onClick={handleStart}
+          >START</button>
+      </div>
+      
+      <div className="settings">
+        <div id="work">
+          <p>Work Duration</p>
+          <button 
+            className="btn-settings" 
+            id="work-plus" 
+            onClick={incrementWork}>+</button>
+          <div><span id="work-min">{workTime}</span> mins</div>
+          <button 
+            className="btn-settings" 
+            id="work-minus"
+            onClick={decrementWork}>-</button>
+        </div>
+        
+        <button id="reset" className="btn">RESET</button>
+        
+        <div id="break">
+          <p>Break Duration</p>
+          <button 
+            className="btn-settings" 
+            id="break-plus"
+            onClick={incrementBreak}>+</button>
+          <div><span id="break-min">{breakTime}</span> mins</div>
+          <button 
+            className="btn-settings" 
+            id="break-minus"
+            onClick={decrementBreak}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/SessionTimer.js
+++ src/SessionTimer.js
@@ -2,49 +2,47 @@ import React, {useState} from 'react'
 import Countdown, { calcTimeDelta } from 'react-countdown';
 
 export default function SessionTimer({handleOnReset, breakDisplay, lengthDisplay}){
+    /* will use setDescription to update session to break when time is up */
+    const [seshName, setSeshname] = useState(lengthDisplay)
     const [descriptionDisplay, setDescription] = useState("Session")
-    const [date, setDate] = useState(Date.now() + (lengthDisplay*60000))
-    const [isSession, setIsSession] = useState(true)
-    const [auto, setAuto] = useState(false)
+    const alarm = 'https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3'
+    function time_convert(num)
+    { 
+     var hours = Math.floor(num / 60);  
+     var minutes = num % 60;
+     var seconds = minutes % 1500;
+     return hours + ":" + minutes + ":" + seconds;         
+   }
 
-    function handleStartStop(){
-        setAuto(!auto)
-        
+   function handleStart(){}
+   function handlePause(){}
+   function endTime(){
+       if(lengthDisplay === 0){
+           /*play alarm */
+           setDescription("Break")
+           setSeshname(breakDisplay)
 
-        /* do I need state to keep track if button has been clicked? if clicked once - play countdown, 
-        if clicked twice (or divisible by 2) then pause countdown?
-        set state to 'isfalse' and if clicked turn it to true - use if statement to decide to play or pause
-    }
 
-   function handleOnComplete(){
-            setSessionDisplay({breakDisplay}) -- this will end up using setdate, which controls start time of break
-            setDescription("Break") /*changes display to state break
-            use isSession to declare true or false - 
-        use true or false to determine setDate of break or length display
-*/        
-    }
+       }
+   }
 
-    return(
+    return( 
         <div id="timer-label">
             <p>{descriptionDisplay}</p>
             <div id="time-left">
-                <Countdown
-                    date={date}
-                    intervalDelay={0}
-                    precision={0}
-                    autoStart={true}
-                    onPause={handleStartStop}
-                    /* onComplete={handleOnComplete} */
-  />
-
+                {lengthDisplay}
             </div>
             <button 
                 id="start_stop"
-                onClick={handleStartStop}>&#10074;&#10074;</button>
+                onClick={handleStart}
+                >&#9654;</button>
+            <button 
+                id="start_stop"
+                onClick={handlePause}
+                >&#10074;&#10074;</button>
             <button 
                 id="reset"
                 onClick={handleOnReset}>&#8634;</button>
-
         </div>
     )
 }
